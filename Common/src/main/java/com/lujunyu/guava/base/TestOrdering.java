c2d70221fb183651a077542d7be1c8b2d5838e5e
lujunyu <lujunyu@58ganji-corp.com>
2017-05-04 15:43:34 +0800

    guava

--- Common/src/main/java/com/lujunyu/guava/base/TestOrdering.java
+++ Common/src/main/java/com/lujunyu/guava/base/TestOrdering.java
@@ -3,27 +3,34 @@ package com.lujunyu.guava.base;
 import java.util.ArrayList;
 import java.util.List;
 
+import com.google.common.base.Function;
 import com.google.common.collect.Ordering;
 
 public class TestOrdering {
 	public static void main(String[] args) {
-		Ordering<String> order = new Ordering<String>(){
+		
+		//创造。
+		//自然排序比较器。str1.compareTo(str2)
+		Ordering<String> natural = Ordering.natural();
+		//使用toStirng排序。s1.toString.compareTo(s2.toString)
+		Ordering<Object> usingToString = Ordering.usingToString();
+		//自定义比较器。
+		Ordering<String> compare = new Ordering<String>(){
 			@Override
 			public int compare(String left, String right) {
-				return left.compareTo(right);
+				return 0;
 			}
-			
 		};
-		List<String> list = new ArrayList<String>();
-		list.add("c");
-		list.add("a");
-		list.add("b");
-		//排序list，并且进行copy
-		List<String> sortedList = order.sortedCopy(list);
-		System.out.println(list);
-		System.out.println(sortedList);
-		//自然排序比较器。
-		Ordering.natural();
+		
+		
+		
+		//链式api,通过类似包装的功能，丰富比较器的功能。
+		Ordering.natural().nullsFirst().onResultOf(new Function<String, String>() {
+			@Override
+			public String apply(String input) {
+				return input;
+			}
+		});
 		
 		
 	}
