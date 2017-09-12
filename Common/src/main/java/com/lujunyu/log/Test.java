00e907a8cd4d3d99c45d4ffc3ce7853478739b43
ljy <784698289@qq.com>
2017-09-12 23:55:29 +0800

    hyrix框架学习

--- Common/src/main/java/com/lujunyu/log/Test.java
+++ Common/src/main/java/com/lujunyu/log/Test.java
@@ -6,9 +6,10 @@ import org.apache.logging.log4j.LogManager;
 public class Test {
 	private static Logger logger = LogManager.getLogger();
 	
-	public static void main(String[] args) {
+	public static void main(String[] args) throws InterruptedException {
 		while(true){
 			logger.info("21312");
+			Thread.sleep(1000);
 		}
 	}
 }
