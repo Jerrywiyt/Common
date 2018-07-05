9dcd4eac234fc993b7780af0830ac941d4d24f22
lujunyu <784698289@qq.com>
2018-07-05 20:53:06 +0800

    学习netty

---
+++ Common/src/main/java/com/lujunyu/netty/study/TimeServer.java
@@ -0,0 +1,37 @@
+package com.lujunyu.netty.study;
+
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.ChannelOption;
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+
+public class TimeServer {
+    public void bind(int port) throws InterruptedException {
+        EventLoopGroup boss = new NioEventLoopGroup();
+        EventLoopGroup work = new NioEventLoopGroup(128);
+        try {
+            ServerBootstrap b = new ServerBootstrap();
+            b.group(boss,work).channel(NioServerSocketChannel.class).option(ChannelOption.SO_BACKLOG,1024).childHandler(new ChildChannelHandler());
+            ChannelFuture f = b.bind(port).sync();
+            f.channel().closeFuture().sync();
+        }finally {
+            boss.shutdownGracefully();
+            work.shutdownGracefully();
+        }
+    }
+    private class ChildChannelHandler extends ChannelInitializer<SocketChannel>{
+
+        @Override
+        protected void initChannel(SocketChannel socketChannel) throws Exception {
+            socketChannel.pipeline().addLast(new TimeServerHandler());
+        }
+    }
+
+    public static void main(String args[]) throws InterruptedException {
+        new TimeServer().bind(8080);
+    }
+}
---
+++ Common/src/main/java/com/lujunyu/netty/study/TimeServerHandler.java
@@ -0,0 +1,29 @@
+package com.lujunyu.netty.study;
+
+import io.netty.buffer.ByteBuf;
+import io.netty.buffer.Unpooled;
+import io.netty.channel.ChannelHandlerAdapter;
+import io.netty.channel.ChannelHandlerContext;
+
+public class TimeServerHandler extends ChannelHandlerAdapter {
+    @Override
+    public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
+        ByteBuf byteBuf = (ByteBuf) msg;
+        byte[] req = new byte[byteBuf.readableBytes()];
+        byteBuf.readBytes(req);
+        String body = new String(req,"utf-8");
+        System.out.println("receive:"+body);
+        ByteBuf resp = Unpooled.copiedBuffer(String.valueOf(System.currentTimeMillis()).getBytes());
+        ctx.write(resp);
+    }
+
+    @Override
+    public void channelReadComplete(ChannelHandlerContext ctx) throws Exception {
+        ctx.flush();
+    }
+
+    @Override
+    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) throws Exception {
+        ctx.close();
+    }
+}
