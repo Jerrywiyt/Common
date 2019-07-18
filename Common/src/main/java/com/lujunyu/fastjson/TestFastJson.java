2dbf9a294c2367614db8c9552686b02637de5db3
lujunyu <784698289@qq.com>
2019-07-18 17:15:42 +0800

    DEV:demo代码

---
+++ Common/src/main/java/com/lujunyu/fastjson/TestFastJson.java
@@ -0,0 +1,9 @@
+package com.lujunyu.fastjson;
+
+import com.alibaba.fastjson.JSON;
+
+public class TestFastJson {
+    public static void main(String args[]){
+        JSON.parse("{\"key\":1}");
+    }
+}
